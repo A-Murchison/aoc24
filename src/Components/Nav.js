ca114479371df646ccaf507547a063b37347a872
Adam Murchison <adam.murchison@hotmail.com>
2024-12-03 16:46:49 +1300

    day3

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,62 +2,61 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Day1 from "../day1/aoc1.js";
 import Day2 from "../day2/aoc2.js";
+import Day3 from "../day3/aoc3.js";
 import HomePage from "./Home.js";
 import "../site.css";
 
 const Nav = () => {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    // Function to toggle dropdown
-    const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
-    };
+  // Function to toggle dropdown
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
-    // Function to close dropdown when focus is lost
-    const closeDropdown = () => {
-        setDropdownOpen(false);
-    };
+  // Function to close dropdown when focus is lost
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
 
-    return (
-        <Router>
-            <nav className="navbar">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li 
-                        onMouseEnter={toggleDropdown} 
-                        onMouseLeave={closeDropdown} 
-                        className="dropdown"
-                    >
-                        <Link to="#" >
-                            Days
-                        </Link>
-                        {dropdownOpen && (
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <Link to="/Day1" >
-                                        Day 1
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/Day2">
-                                        Day 2
-                                    </Link>
-                                </li>
-                            </ul>
-                        )}
-                    </li>
-                </ul>
-            </nav>
+  return (
+    <Router>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li
+            onMouseEnter={toggleDropdown}
+            onMouseLeave={closeDropdown}
+            className="dropdown"
+          >
+            <Link to="#">Days</Link>
+            {dropdownOpen && (
+              <ul className="dropdown-menu">
+                <li>
+                  <Link to="/Day1">Day 1</Link>
+                </li>
+                <li>
+                  <Link to="/Day2">Day 2</Link>
+                </li>
+                <li>
+                  <Link to="/Day3">Day 3</Link>
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </nav>
 
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/Day1" element={<Day1 />} />
-                <Route path="/Day2" element={<Day2 />} />
-            </Routes>
-        </Router>
-    );
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Day1" element={<Day1 />} />
+        <Route path="/Day2" element={<Day2 />} />
+        <Route path="/Day3" element={<Day3 />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default Nav;
---
+++ src/day3/aoc3.js
@@ -0,0 +1,69 @@
+import inputText from "./day3.txt";
+import "../site.css";
+import { GetTextFromFile } from "../Helpers/fileReader";
+import React, { useEffect, useState } from "react";
+import Template from "../Components/Template";
+
+export function Day3() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [output2, setOutput2] = useState("");
+  useEffect(() => {
+    GetTextFromFile(inputText).then((text) => {
+      setInput(text);
+    });
+  }, []);
+
+  useEffect(() => {
+    partOne(input).then((result) => {
+      setOutput(result);
+    });
+  });
+  useEffect(() => {
+    partTwo(input).then((result) => {
+      setOutput2(result);
+    });
+  });
+
+  let aoc = {};
+  aoc.dayText = "Day 3";
+  aoc.input = input;
+  aoc.output = output;
+  aoc.output2 = output2;
+  aoc.partOne = partOne.toString();
+  aoc.partTwo = partTwo.toString();
+
+  return <Template aoc={aoc}> </Template>;
+}
+
+// =====PART ONE =====
+
+function partOne(textInput) {
+  let outputVal = 0;
+  if (textInput === "") {
+    return new Promise((resolve, reject) => {
+      resolve(outputVal);
+    });
+  }
+
+  return new Promise((resolve, reject) => {
+    resolve(outputVal);
+  });
+}
+
+// =====PART TWO =====
+
+function partTwo(textInput) {
+  let outputVal = 0;
+  if (textInput === "") {
+    return new Promise((resolve, reject) => {
+      resolve(outputVal);
+    });
+  }
+
+  return new Promise((resolve, reject) => {
+    resolve(outputVal);
+  });
+}
+
+export default Day3;
