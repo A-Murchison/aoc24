22719c222f260077a38c08ace8f852a57ce8052e
Adam Murchison <adam.murchison@hotmail.com>
2024-12-03 14:55:00 +1300

    new tree

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import logo from "../logo.svg";
 
 const HomePage = () => {
-    return   (    
+  return (
     <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      Adam Murchison's Advent of Code 2024
-    </header>
-  </div>);
-  };
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Adam Murchison's Advent of Code 2024
+      </header>
+    </div>
+  );
+};
 
-  export default HomePage;
+export default HomePage;
